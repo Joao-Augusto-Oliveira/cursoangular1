8206b835f0b3f51ff6dc0ecbaa27076a8cb15e60
Joao Augusto Oliveira dos Santos <joaoaugustofurg@gmail.com>
2021-09-01 23:24:22 -0300

    aula 13: Input properties

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CursosModule } from './cursos/cursos.module';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { InputPropertyComponent } from './input-property/input-property.component';
 
 @NgModule({
-  declarations: [AppComponent, DataBindingComponent],
+  declarations: [AppComponent, DataBindingComponent, InputPropertyComponent],
   imports: [BrowserModule, AppRoutingModule, CursosModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
